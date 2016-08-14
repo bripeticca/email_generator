fab1005f4678324a22b2963b1ca14df90171e415
bripeticca <bripeticca@gmail.com>
2016-08-14 11:53:18 -0400

    Algorithm Update

--- email_generator.c
+++ email_generator.c
@@ -5,11 +5,6 @@
 
 void email_generator(int num) {
 
-  int count = num;
-  string email_begin;
-  string email_end; // make sure email end includes the dot
-  string *emails = malloc(sizeof(string) * num);
-
   const char alphanumeric[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
   const int len_alpha = sizeof(alphanumeric);
 
@@ -17,6 +12,7 @@ void email_generator(int num) {
   int len = 26;
 
   while (num > 0) {
+
     int placeholder = 0;
 
     for(int i = 0; i < 10; ++i) {
@@ -24,22 +20,25 @@ void email_generator(int num) {
       placeholder = i;
     }
 
-    new_email[placeholder] = "@";
+    new_email[placeholder] = '@';
 
     for(int z = 0; z < 10; ++z){
-
-      ++placeholder
+      ++placeholder;
+      new_email[placeholder] = alphanumeric[rand() % len_alpha - 1];
     }
 
-    string new_email_string;
-    strcpy(new_email_string, new_email, len);
-    new_email_string[len] = '\0';
-    emails[num - 1] = new_email_string;
-  }
+    ++placeholder;
 
-  for(int x = 0; x < count; ++x) {
-    printf("%s", emails[x]);
-  }
+    new_email[placeholder] = '.';
 
-  free(emails);
+    for(int x = 0; x < 3; ++x){
+      ++placeholder;
+      new_email[placeholder] = alphanumeric[rand() % len_alpha - 1];
+    }
+
+    for(int y = 0; y < len; ++y) {
+      printf("%c", new_email[y]);
+    }
+    printf("\n");
+  }
   }
--- email_generator_main.c
+++ email_generator_main.c
@@ -6,18 +6,19 @@
 #include "email_generator.h"
 
 int main(void) {
-  int num;
+  int *num = 0;
   bool valid = true;
 
-  printf("Please insert the number of emails you wish to generate.")
+  printf("Please insert the number of emails you wish to generate.");
 
   while(valid) {
 
-  if (scanf("%d", num) == 1 || num == 0 || num > 100) {
-    return email_generator(num);
+  if (scanf("%d", num) == 1) {
+    email_generator(*num);
+    return 1;
     valid = false;
   } else {
-    printf("Insert a valid input - should be > 0 and < 100.")
+    printf("Insert a valid input - should be > 0 and < 100.");
   }
 }
 }
