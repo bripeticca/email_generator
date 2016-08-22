b01e2696547ff18c6fb07c3452169266c1dc4b51
Brianna <bripeticca@gmail.com>
2016-08-22 16:00:55 -0400

    Fixed error with num pointer

--- main.c
+++ main.c
@@ -6,15 +6,14 @@
 #include "email_generator.h"
 
 int main(void) {
-  int *num = 0;
+  int num = 0;
   bool valid = true;
 
-  printf("Please insert the number of emails you wish to generate.");
+  printf("Please insert the number of emails you wish to generate.\n");
 
   while(valid) {
-
-  if (scanf("%d", num) == 1) {
-    email_generator(*num);
+  if (scanf("%d", &num) == 1) {
+    email_generator(num);
     valid = false;
   } else {
     printf("Insert a valid input - should be > 0 and < 100.");
