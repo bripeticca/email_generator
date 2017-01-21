e80d66ee90f3bd640772982c1c7858c708cdcffb
bripeticca <bripeticca@gmail.com>
2017-01-21 11:44:34 -0500

    Can now customize size of emails.

--- email_generator.c
+++ email_generator.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "email_generator.h"
 
 void email_generator(int num) {
@@ -10,29 +11,42 @@ void email_generator(int num) {
   // the null terminator.
 
   char com[] = ".com";
-  char email[26];
-  char len = 26;
+  int midpoint; // point before "@"
+  int end;	// point after "@"
 
-  int midpoint = 10;  // the point before '@'
-  int end = 21;       // represents the point just before '.com'
+  printf("Please enter the number of characters you'd like before '@' and after '@'.\n");
+
+  while(true) {
+      if(!(scanf("%d %d", &midpoint, &end))) {
+	  printf("Please enter a valid number of emails.");
+      } else {
+	  break;
+      }
+  }
+  
+  end += midpoint; // fixes offset
+
+  int len = midpoint + end + 5; // number of chars in midpoint + end + "@" + ".com"
+  
+  char *email = malloc((sizeof(char)) * len);
 
   while (num > 0) {
 
     int placeholder = 0;
 
     for(int i = 0; i < len; ++i){
-      if ((i < midpoint) || (i > midpoint && i < end)){
+      if ((i < midpoint) || (i > midpoint && i <= end)){
         email[i] = alphanumeric[rand() % len_alpha];
       } else if (i == midpoint) {
         email[i] = '@';
+
       } else {
         email[i] = com[placeholder];
         ++placeholder;
       }
     }
-
     printf("%s\n", email);
-
     --num;
   }
+  free(email);
 }
